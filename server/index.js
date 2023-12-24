d468021f52752e0129e617b2d810db7c4c415433
Mohana Misra <iammohanamisra@gmail.com>
2023-12-24 15:28:10 +0530

    Added room joining and message sending code

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const socketio = require('socket.io');
 const http = require('http');
 const cors = require('cors');
 
+const {addUser, removeUser, getUser, getUsersInRoom} = require('./users');
+
 const PORT = process.env.port || 5000;
 
 const router = require('./router');
@@ -14,12 +16,23 @@ const io = socketio(server);
 //THERE YOU GO
 
 io.on('connection', (socket) => {
-    console.log('We have a new connection!');
-
     socket.on('join', ({name, room}, callback) => {
-        console.log(name, room);
+        const {error, user} = addUser({id: socket.id, name, room});
 
-    })
+        if(error) return callback(error);
+        socket.emit('message', {user: 'admin', text: `Hi ${user.name}! You have joined room ${user.room}`});
+        socket.broadcast.to(user.room).emit('message', {user: 'admin', text: `${user.name} has joined, everyone say hi!`});
+
+        socket.join(user.room);
+        callback();
+    });
+
+    socket.on('sendMessage', (message, callback) => {
+        const user = getUser(socket.id);
+        io.to(user.room).emit('message', {user: user.name, text: message});
+        callback();
+    });
+
     socket.on('disconnect', () => {
         console.log('Aw man they left...');
     })
