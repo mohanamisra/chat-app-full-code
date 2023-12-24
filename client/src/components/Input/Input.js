8707abf427766885da514a2a8d127712dbb0b35e
Mohana Misra <iammohanamisra@gmail.com>
2023-12-24 22:32:31 +0530

    removed onclick

--- client/src/components/Input/Input.js
+++ client/src/components/Input/Input.js
@@ -6,7 +6,7 @@ const Input = ({message, handleMessageChange, handleMessageSend}) => {
         <form className = 'message-form'>
             <input className = 'message-input' type = 'text' placeholder = 'Type your message...' value = {message}
                    onChange = {handleMessageChange} onKeyPress = {handleMessageSend}/>
-            <button className = 'sendButton' onClick = {handleMessageSend}>Send</button>
+            <button className = 'sendButton'>Send</button>
         </form>
     )
 }
