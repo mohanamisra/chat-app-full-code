c37d52a041f52578dd9d617a44dd3b57b223e2bd
Mohana Misra <iammohanamisra@gmail.com>
2023-12-25 18:21:41 +0530

    fixing reload

--- client/src/Chat.js
+++ client/src/Chat.js
@@ -1,44 +1,45 @@
-import React from 'react';
-import {useState, useEffect} from 'react';
-import queryString from 'query-string';
-import { useNavigate } from 'react-router-dom';
-import io from 'socket.io-client';
+import React from "react";
+import { useState, useEffect } from "react";
+import queryString from "query-string";
+import { useNavigate } from "react-router-dom";
+import io from "socket.io-client";
 import InfoBar from "./components/InfoBar/InfoBar";
 import Input from "./components/Input/Input";
 import Messages from "./components/Messages/Messages";
 import TextContainer from "./components/TextContainer/TextContainer";
 
-import './Chat.css';
+import "./Chat.css";
 const ENDPOINT = 'https://cyberpunkchat.onrender.com';
-
-let socket;
+const socket = io(ENDPOINT, {
+    transports: ["websocket"],
+}); // ! now defined globally
 
 const Chat = () => {
     const navigate = useNavigate();
-    const [name, setName] = useState('');
-    const [room, setRoom] = useState('');
+    const [name, setName] = useState("");
+    const [room, setRoom] = useState("");
     const [messages, setMessages] = useState([]);
-    const [message, setMessage] = useState('');
-    const [users, setUsers] = useState('');
+    const [message, setMessage] = useState("");
+    const [users, setUsers] = useState("");
 
     useEffect(() => {
-        const {name, room} = queryString.parse(window.location.search);
-        socket = io(ENDPOINT, {transports:['websocket']});
-        if(!name || !room) {
-            console.log('I forgot...');
+        const info = queryString.parse(window.location.search);
+        const query_name = info.name; // ! to prevent name conflicts
+        const query_room = info.room; // ! to prevent name conflicts
+        console.log(query_name, query_room);
+        if (!query_name || !query_room) {
+            console.log("I forgot...");
+        } else {
+            setName(query_name);
+            setRoom(query_room);
+            socket.emit("join", { name: query_name, room: query_room }, () => {});
         }
-        else {
-            setName(name);
-            setRoom(room);
-            socket.emit('join', {name, room}, () => {
-            });
-        }
-    }, [ENDPOINT, navigate]);
+    }, [navigate]);
 
     useEffect(() => {
-        socket.on('message', (message) => {
+        socket.on("message", (message) => {
             setMessages([...messages, message]);
-        })
+        });
         socket.on("roomData", ({ users }) => {
             setUsers(users);
         });
@@ -46,20 +47,23 @@ const Chat = () => {
 
     const sendMessage = (event) => {
         event.preventDefault();
-        if(message)
-            socket.emit('sendMessage', message, () => setMessage(''));
-    }
+        if (message) socket.emit("sendMessage", message, () => setMessage(""));
+    };
 
-    return(
-        <div className = 'outerContainer'>
-            <TextContainer users = {users}/>
-            <div className = 'container'>
-                <InfoBar room = {room}/>
-                <Messages messages = {messages} name = {name}/>
-                <Input message = {message} setMessage={setMessage} sendMessage={sendMessage}/>
+    return (
+        <div className="outerContainer">
+            <TextContainer users={users} />
+            <div className="container">
+                <InfoBar room={room} />
+                <Messages messages={messages} name={name} />
+                <Input
+                    message={message}
+                    setMessage={setMessage}
+                    sendMessage={sendMessage}
+                />
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default Chat;
--- server/users.js
+++ server/users.js
@@ -1,14 +1,15 @@
 const users = [];
 
 const addUser = ({id, name, room}) => {
+    if(!name || !room) return;
     name = name.trim().toLowerCase();
     room = room.trim().toLowerCase();
 
     const existingUser = users.find(user => user.room === room && user.name === name);
     if(existingUser) {
         return {
-            error: 'Username is taken in this room.'
-        }
+            error: 'Username is taken in this room.',
+        };
     }
     const user = {id, name, room};
     users.push(user);
